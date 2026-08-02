bea1c50730fcd59456194b35efaeb4c1270e2acc
Raul Politi <raul_politi@hotmail.com>
2026-08-02 12:31:54 -0300

    feat: fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 
-import axios from 'axios'
-
-import Counter from './Counter'
-import { useAxios } from './hooks/useAxios'
-
-
-
 function App() {
 
-  // const {data, error, loading} = useAxios({
-  //   instance: axios,
-  //   method: 'get',
-  //   url: 'https://rickandmortyapi.com/api/character',
-  //   enabled: true
-  // })
-  //console.log('data', data)
+
   return (
     <>
       <h2>My Hooks Lib 📖</h2>
       <br />
-      <Counter/>
+     
     </>
   )
 }
