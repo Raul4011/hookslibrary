b777efd75f3f0ca95133ae5c53137b1426a08819
Raul Politi <raul_politi@hotmail.com>
2026-08-06 20:48:04 -0300

    chore: adding new hooks and tests

--- src/hooks/__tests__/useToogle.test.ts
+++ src/hooks/__tests__/useToogle.test.ts
@@ -1,6 +1,6 @@
 
 import {act,renderHook} from '@testing-library/react'
-import {describe,expect,it,vi} from 'vitest'
+import {describe,expect,it} from 'vitest'
 
 import { useToogle } from '../useToogle'
 
